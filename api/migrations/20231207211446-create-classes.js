ed9df73578ae1db486c15420b7edfa2ddda6bbef
Kevin Araújo <kfa_34@hotmail.com>
2023-12-11 12:20:45 -0300

    feat: Created seed files to Levels, Classes and Registrations tables. Renamed Person seed file to People.

--- api/migrations/20231207211446-create-classes.js
+++ api/migrations/20231207211446-create-classes.js
@@ -12,6 +12,16 @@ module.exports = {
       start_date: {
         type: Sequelize.DATEONLY
       },
+      teacher_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'People', key: 'id'}
+      },
+      level_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Levels', key: 'id'}
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
